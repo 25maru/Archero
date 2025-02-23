dcb405a8d854398886fdd721a5315b245ef0c404
leeejinho <86703795+leeejinho@users.noreply.github.com>
2025-02-23 20:53:05 +0900

    Feat: 인벤토리 UI 기능 추가

---
+++ Assets/Scripts/LJH/InventoryUI.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textGold;
+    [SerializeField] TextMeshProUGUI textDiamond;
+    [SerializeField] TextMeshProUGUI textName;
+    [SerializeField] TextMeshProUGUI textHealth;
+    [SerializeField] TextMeshProUGUI textAttack;
+    [SerializeField] Button btnReturn;
+    [SerializeField] GridLayoutGroup gridItem;
+
+    Image[] itemList;
+
+    private void Start()
+    {
+        // 돌아가기 버튼 클릭 이벤트 연결
+        btnReturn.onClick.AddListener(OnClickReturn);
+
+        // ItemView 이미지들 참조
+        itemList = gridItem.GetComponentsInChildren<Image>();
+    }
+
+    private void OnClickReturn()
+    {
+        // 뒤로 가기
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void UpdateGold(float gold)
+    {
+        textGold.text = gold.ToString();
+    }
+
+    public void UpdateDiamond(float diamond)
+    {
+        textDiamond.text = diamond.ToString();
+    }
+
+    public void UpdateName(string name)
+    {
+        textName.text = name;
+    }
+
+    public void UpdateHealth(float maxHealth)
+    {
+        textHealth.text = maxHealth.ToString();
+    }
+
+    public void UpdateAttack(float attack)
+    {
+        textAttack.text = attack.ToString();
+    }
+
+    public void UpdateItemList(List<Item> items)
+    {
+        for(int i = 0; i < itemList.Length; i++)
+        {
+            itemList[i].sprite = items[i].ItemSprite.sprite;
+        }
+    }
+}
+
+// 임시 아이템 클래스
+public class Item : MonoBehaviour
+{
+    SpriteRenderer itemSprite;
+    public SpriteRenderer ItemSprite { get; private set; }
+}
