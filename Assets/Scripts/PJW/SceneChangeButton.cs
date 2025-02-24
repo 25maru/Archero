6ea38b34338449802b4ed5c59ece33bb1fb37c9f
25maru <qkrwldnjs6694@gmail.com>
2025-02-24 16:21:51 +0900

    Feat: 타이틀 씬 버튼 연결 구현
    - SceneChangeButton 스크립트 사용

---
+++ Assets/Scripts/PJW/SceneChangeButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChangeButton : MonoBehaviour
+{
+    [SerializeField] private GameManager.GameState targetState;
+    private Button button; // 버튼 컴포넌트
+
+    private void Start()
+    {
+        if (button == null)
+            button = GetComponent<Button>(); // 버튼이 설정되지 않았다면 자동 할당
+
+        if (button != null)
+        {
+            button.onClick.RemoveAllListeners(); // 기존 이벤트 제거
+            button.onClick.AddListener(() => GameManager.Instance.ChangeState(targetState));
+        }
+    }
+}
