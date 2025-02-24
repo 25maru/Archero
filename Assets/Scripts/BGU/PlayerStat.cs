83e625ad94418f808869e2c64bacf23527d39c53
100ZADA <5528asd@gmail.com>
2025-02-24 14:37:10 +0900

    refactor: 코드 삭제
    PlayerStatUI.cs 삭제
    Canvas 수치 조정

--- Assets/Scripts/BGU/PlayerStat.cs
+++ Assets/Scripts/BGU/PlayerStat.cs
@@ -6,33 +6,9 @@ public class PlayerStat : MonoBehaviour
 {
     public PlayerData playerData;
 
-    public string Name = "Test";                                     // 플레이어 이름
-    public int HP = 100;                                             // 체력
-    public int MaxHP = 100;                                          // 최대 체력
-    public bool IsDead = false;                                      // 사망조건
-
-    public int Gold = 0;                                             // 골드
-    public int Diamond = 0;                                          // 다이아몬드
-
-    [Range(1, 20)][SerializeField] public int speed = 10;            // 캐릭터 속도
-    public int Speed
-    {
-        get { return speed; }
-        set { speed = Mathf.Clamp(value, 1, 20); }                   // 값이 1~20 범위를 벗어나지 않도록 제한
-    }
-
-    [Range(1, 5)][SerializeField] public int attackDamage = 3;       // 캐릭터 공격력
-    public int AttackDamage
-    {
-        get { return attackDamage; }
-        set { attackDamage = Mathf.Clamp(value, 1, 5); }             // 값이 1~5 범위를 벗어나지 않도록 제한
-    }
-    [Range(1, 5)][SerializeField] public int attackSpeed = 3;        // 공격 속도
-    public int AttackSpeed
-    {
-        get { return attackSpeed; }
-        set { attackSpeed = Mathf.Clamp(value, 1, 5); }              // 값이 1~5 범위를 벗어나지 않도록 제한
-    }
+    public int HP;
+    public int MaxHP;
+    public bool IsDead = false;
 
     void Start()
     {
