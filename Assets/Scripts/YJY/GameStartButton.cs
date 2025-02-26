37ffb8b01e22c4035533e5dc8bf1a3913fe87e00
Yun-Jiyeol <yir0612@gmail.com>
2025-02-26 12:08:49 +0900

    Feat: 피로도 감소 로직 생성

--- Assets/Scripts/YJY/GameStartButton.cs
+++ Assets/Scripts/YJY/GameStartButton.cs
@@ -8,16 +8,25 @@ public class GameStartButton : MonoBehaviour
     [SerializeField] private GameManager.GameState targetState;
     //[SerializeField] private SelectManager.SelectState selectState;
     private Button button; // 버튼 컴포넌트
+    public TiredManager tiredManager;
+    bool isStart = false;
 
     private void Start()
     {
         if (button == null)
             button = GetComponent<Button>(); // 버튼이 설정되지 않았다면 자동 할당
 
-        if (button != null)
+        if (button != null && !isStart)
         {
+            isStart = true;
             button.onClick.RemoveAllListeners(); // 기존 이벤트 제거
-            button.onClick.AddListener(() => GameManager.Instance.ChangeState(targetState));
+            button.onClick.AddListener(() => tiredManager.MinusTired());
+            button.onClick.AddListener(() => Invoke("InvokeStartGame",1f));
         }
+    }
+
+    void InvokeStartGame()
+    {
+        GameManager.Instance.ChangeState(targetState);
     }
 }
