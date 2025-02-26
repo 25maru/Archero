b6c3f2730e2f22d02fca9613b7c63a2b5d2705fd
Yun-Jiyeol <yir0612@gmail.com>
2025-02-26 11:59:47 +0900

    Feat: 플레이어 골드,다이야 보이도록 제작

---
+++ Assets/Scripts/YJY/DiaShow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DiaShow : MonoBehaviour
+{
+    public TextMeshProUGUI Dia;
+    public PlayerData playerData;
+
+    private void Start()
+    {
+        Show();
+    }
+
+    public bool Use(int much) //사용 또는 획득, 사용 가능 유무
+    {
+        if (playerData.Diamond < much) return false; //골드가 부족할 시
+
+        playerData.Diamond -= much;
+        Show();
+        return true;
+    }
+
+    void Show()
+    {
+        Dia.text = playerData.Diamond.ToString();
+    }
+}
---
+++ Assets/Scripts/YJY/GoldShow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GoldShow : MonoBehaviour
+{
+    public TextMeshProUGUI Gold;
+    public PlayerData playerData;
+
+    private void Start()
+    {
+        Show();
+    }
+
+    public bool Use(int much) //사용 또는 획득, 사용 가능 유무
+    {
+        if(playerData.Gold < much) return false; //골드가 부족할 시
+
+        playerData.Gold -= much;
+        Show();
+        return true;
+    }
+
+    void Show()
+    {
+        Gold.text = playerData.Gold.ToString();
+    }
+}
