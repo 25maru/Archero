2c1d4e839ae71bed5a7e467ed060e9649be09624
Yun-Jiyeol <yir0612@gmail.com>
2025-02-27 15:08:00 +0900

    Feat: NormalStage1 만들기
    가장 기본적인

--- Assets/Scripts/YJY/NormalStageController.cs
+++ Assets/Scripts/YJY/NormalStageController.cs
@@ -5,35 +5,71 @@ using UnityEngine;
 public class NormalStageController : MonoBehaviour
 {
     public bool isRed = true;
-    public StageButtonAction Red;
-    public StageButtonAction Blue;
+    public List<StageButtonAction> Red;
+    public List<StageButtonAction> Blue;
 
-    public GameObject RedParticle;
-    public GameObject BlueParticle;
+    public List<GameObject>  RedParticle;
+    public List<GameObject> BlueParticle;
 
-    public WindZone Redwind;
-    public WindZone Bluewind;
+    public List<WindZone>  Redwind;
+    public List<WindZone> Bluewind;
 
     public void ChangeisRed()
     {
         isRed = !isRed;
         if (isRed)//빨간 버튼이 눌릴 시
         {
-            Red.DownThisButton();
-            Blue.UpThisButton();
-            RedParticle.SetActive(true);
-            BlueParticle.SetActive(false);
-            Redwind.isUP = true;
-            Bluewind.isUP = false;
+            foreach (StageButtonAction r in Red)
+            {
+                r.DownThisButton();
+            }
+            foreach (StageButtonAction b in Blue)
+            {
+                b.UpThisButton();
+            }
+            foreach (GameObject rp in RedParticle)
+            {
+                rp.SetActive(true);
+            }
+            foreach (GameObject bp in BlueParticle)
+            {
+                bp.SetActive(false);
+            }
+            foreach (WindZone rw in Redwind)
+            {
+                rw.isUP = true;
+            }
+            foreach (WindZone bw in Bluewind)
+            {
+                bw.isUP = false;
+            }
         }
         else
         {
-            Red.UpThisButton();
-            Blue.DownThisButton();
-            RedParticle.SetActive(false);
-            BlueParticle.SetActive(true);
-            Redwind.isUP = false;
-            Bluewind.isUP = true;
+            foreach (StageButtonAction r in Red)
+            {
+                r.UpThisButton();
+            }
+            foreach (StageButtonAction b in Blue)
+            {
+                b.DownThisButton();
+            }
+            foreach (GameObject rp in RedParticle)
+            {
+                rp.SetActive(false);
+            }
+            foreach (GameObject bp in BlueParticle)
+            {
+                bp.SetActive(true);
+            }
+            foreach (WindZone rw in Redwind)
+            {
+                rw.isUP = false;
+            }
+            foreach (WindZone bw in Bluewind)
+            {
+                bw.isUP = true;
+            }
         }
     }
 }
