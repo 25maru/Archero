41fabaf7d14489a6c9c0195c15c9ee368772eb9d
Yun-Jiyeol <yir0612@gmail.com>
2025-02-28 09:28:59 +0900

    Feat: 게임종료 버튼 제작

---
+++ Assets/Scripts/YJY/EscapeButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeButton : MonoBehaviour
+{
+    public void ClickEscapeButton()
+    {
+        Application.Quit();
+    }
+}
